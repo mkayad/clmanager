bc42c9fca0046349386930cbbf3d26ee0f0aaf9d
Moussa Kayad <mkayad@gmail.com>
2020-03-12 11:40:57 +0000

    test 3

--- src/main/java/com/example/demo/AdminController.java
+++ src/main/java/com/example/demo/AdminController.java
@@ -11,7 +11,7 @@ public class AdminController {
     public String name;
     @GetMapping("/list")
     public String index(){
-        System.out.println("Application with current profile "+name);
+        System.out.println("Application with current profile 0"+name);
         return "index";
     }
 }
